0578af1241f982c75697cb00405fce2efb4d633a
dicotin <dvdicotin@gmail.com>
2023-06-22 22:20:15 +0300

    part_10

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import Technologies from "./Technologies";
+import Header from "./Header";
+import Footer from "./Footer";
+
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+            <Footer/>
+        </div>
+    );
 }
+
+
+
+
 
 export default App;
